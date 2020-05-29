36ce1a1481e2a5b0bcf7e6c112e9c93a9fd7aeb5
BastaAditya <54901460+BastaAditya@users.noreply.github.com>
2020-05-29 15:48:54 +0530

    Create add_faculty.js

---
+++ js/add_faculty.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
+    
+
+    $("#generate").click(function(event){
+        event.preventDefault();
+
+        var id   = $("input[name=courseid]").val();
+        var slot = $("input[name=slot]").val();
+        var fid = getUrlParameter('facultyid');
+
+        var str = fid + id + slot;
+        $("input[name=classroom]").val(str); 
+    });
+
+    let slotarray = ["A1", "A2","B1", "B2","C1", "C2","D1", "D2","E1", "E2","F1", "F2","G1", "G2"];
+    $("form").submit(function(event){
+        event.preventDefault();
+
+        var id   = $("input[name=courseid]").val();
+        var slot = $("input[name=slot]").val();
+        var fid = getUrlParameter('facultyid');
+        var school = getUrlParameter('school');
+        var classroom = $("input[name=classroom]").val();
+
+        if(!slotarray.includes(slot))
+        {
+            $("#client").css('display', 'block');
+            setTimeout(function(){
+                $("#client").css('display', 'none');
+            }, 2010);
+        }
+        else{
+            var formData = {
+                'id'         : id,
+                'slot'       : slot,
+                'facultyid'  : fid,
+                'school'     : school,
+                'classroom'  : classroom
+            };
+
+            $.ajax({
+                type     : "POST",
+                url      : "php/create2.php",
+                data     : formData,
+                datatype : "text"
+            }).done(function(response){
+                if(response == "Succesfull")
+                {
+                    window.location.replace("start_faculty.html?facultyid="+fid+"&school="+school);
+                }
+            });
+        }
+
+    })
+ });
