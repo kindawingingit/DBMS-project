cb0c5707b54ba32bbf39474e3db41240d54af1fd
BastaAditya <54901460+BastaAditya@users.noreply.github.com>
2020-05-29 15:52:59 +0530

    Create accept.php

---
+++ php/accept.php
@@ -0,0 +1,56 @@
+<?php
+
+function val($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$sid = val($_POST['studentid']);
+$groupid = val($_POST['groupid']);
+$school = val($_POST['school']);
+$cid = val($_POST['courseid']);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "university";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error)
+die("Connection failed: " .$conn->connect_error);
+
+    $sql = "DELETE FROM ". $school ."_groups_applications WHERE groupid = '$groupid' AND applications = '$sid' ";
+
+    if($conn->query($sql) === TRUE)
+    {
+        $sql = "UPDATE ". $school ."_classrooms SET GroupID = '$groupid' WHERE studentid = '$sid' ";
+
+        if($conn->query($sql) === TRUE)
+        {
+            $newgroupid = $groupid . $sid;
+            $sql = "UPDATE ". $school ."_classrooms SET GroupID = '$newgroupid' WHERE groupid = '$groupid' ";
+
+            if($conn->query($sql) === TRUE)
+            {
+                $sql = "UPDATE ". $school ."_groups SET GroupID = '$newgroupid' WHERE groupid = '$groupid' ";
+                
+                if($conn->query($sql) === TRUE)
+                    echo "Successfull";
+                else
+                    echo "Query: ". $sql ." Error: ". $conn->error;
+            }
+            else
+                echo "Query: ". $sql ." Error: ". $conn->error;
+        }
+        else
+            echo "Query: ". $sql ." Error: ". $conn->error;
+    }
+    else
+        echo "Query: ". $sql ." Error: ". $conn->error;
+    
+    $conn->close();
+?>
